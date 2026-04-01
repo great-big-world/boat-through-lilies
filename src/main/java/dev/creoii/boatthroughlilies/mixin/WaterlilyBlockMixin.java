64be310842e300cd47cd29e5947d293be386d68b
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2026-03-31 20:15:08 -0400

    26.1 port

--- src/main/java/dev/creoii/boatthroughlilies/mixin/WaterlilyBlockMixin.java
+++ src/main/java/dev/creoii/boatthroughlilies/mixin/WaterlilyBlockMixin.java
@@ -3,8 +3,8 @@ package dev.creoii.boatthroughlilies.mixin;
 import net.minecraft.core.BlockPos;
 import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.LilyPadBlock;
 import net.minecraft.world.level.block.VegetationBlock;
-import net.minecraft.world.level.block.WaterlilyBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.EntityCollisionContext;
@@ -12,7 +12,7 @@ import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(WaterlilyBlock.class)
+@Mixin(LilyPadBlock.class)
 public abstract class WaterlilyBlockMixin extends VegetationBlock {
     protected WaterlilyBlockMixin(Properties properties) {
         super(properties);
@@ -20,7 +20,7 @@ public abstract class WaterlilyBlockMixin extends VegetationBlock {
 
     @Override
     protected VoxelShape getCollisionShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
-        if (collisionContext instanceof EntityCollisionContext entityCollisionContext && entityCollisionContext.getEntity() != null && entityCollisionContext.getEntity().getType().is(EntityTypeTags.BOAT)) {
+        if (collisionContext instanceof EntityCollisionContext entityCollisionContext && entityCollisionContext.getEntity() != null && entityCollisionContext.getEntity().is(EntityTypeTags.BOAT)) {
             return Shapes.empty();
         }
         return super.getCollisionShape(blockState, blockGetter, blockPos, collisionContext);
