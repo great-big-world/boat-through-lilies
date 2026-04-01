dc1ab674149f3dd9e6479a140d507eb5b72a4378
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2026-03-31 20:19:47 -0400

    1.21.1 backport

--- src/main/java/dev/creoii/boatthroughlilies/mixin/WaterlilyBlockMixin.java
+++ src/main/java/dev/creoii/boatthroughlilies/mixin/WaterlilyBlockMixin.java
@@ -1,10 +1,10 @@
 package dev.creoii.boatthroughlilies.mixin;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.tags.EntityTypeTags;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.LilyPadBlock;
-import net.minecraft.world.level.block.VegetationBlock;
+import net.minecraft.world.level.block.BushBlock;
+import net.minecraft.world.level.block.WaterlilyBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.EntityCollisionContext;
@@ -12,17 +12,21 @@ import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(LilyPadBlock.class)
-public abstract class WaterlilyBlockMixin extends VegetationBlock {
+@Mixin(WaterlilyBlock.class)
+public abstract class WaterlilyBlockMixin extends BushBlock {
     protected WaterlilyBlockMixin(Properties properties) {
         super(properties);
     }
 
     @Override
     protected VoxelShape getCollisionShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
-        if (collisionContext instanceof EntityCollisionContext entityCollisionContext && entityCollisionContext.getEntity() != null && entityCollisionContext.getEntity().is(EntityTypeTags.BOAT)) {
+        if (collisionContext instanceof EntityCollisionContext entityCollisionContext && entityCollisionContext.getEntity() != null && isBoat(entityCollisionContext.getEntity().getType())) {
             return Shapes.empty();
         }
         return super.getCollisionShape(blockState, blockGetter, blockPos, collisionContext);
+    }
+
+    private static boolean isBoat(EntityType<?> entityType) {
+        return entityType == EntityType.BOAT || entityType == EntityType.CHEST_BOAT;
     }
 }
